fb72019da92a7936eb6c5bc26c367aa0c0e20c8c
Bowon Han <rabbitlin@naver.com>
2024-01-28 14:48:28 +0900

    [Init] initialize setting

---
+++ snaptime/Pods/Target Support Files/Pods-snaptime/Pods-snaptime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_snaptime : NSObject
+@end
+@implementation PodsDummy_Pods_snaptime
+@end
---
+++ snaptime/snaptime/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  snaptime
+//
+//  Created by Bowon Han on 1/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
