d11f3c659b87408eb505c2e31ae4152b1f760646
Bowon Han <rabbitlin@naver.com>
2024-03-09 19:33:36 +0900

    [Feat] ProfileSettingItemView 구현

---
+++ snaptime/Snaptime/Profile/View/CustomComponents/ProfileSettingItemView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSettingItemView.swift
+//  Snaptime
+//
+//  Created by Bowon Han on 3/9/24.
+//
+
+import Foundation
---
+++ snaptime/Snaptime/Profile/View/CustomComponents/ProfileSettingView.swift
@@ -0,0 +1,13 @@
+//
+//  SettingView.swift
+//  Snaptime
+//
+//  Created by Bowon Han on 3/9/24.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileSettingView: UIView {
+    
+}
